c08a77b8722a9d09312f25b507507327c4564930
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-02-02 22:58:01 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '50a23a495afeb4ed05f22d443581a88d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f02b3c51-63d0-4d6e-a6b1-34249421a558'
+};
