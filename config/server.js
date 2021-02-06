f0a8e942f977486576756e0f88a2c4a01f13171a
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-02-06 14:31:01 -0400

    deploy develop strapi stage

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+   url: 'https://tertuliapp-api.herokuapp.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '50a23a495afeb4ed05f22d443581a88d'),
